0f1c016b6baeb57c93b00c6efec14e917f1403d0
vano03voin <lena0605810@gmail.com>
2023-10-14 23:35:56 +0300

    Fix xml xsd declaration

--- app/pkg/xml_tools/tools.py
+++ app/pkg/xml_tools/tools.py
@@ -11,13 +11,15 @@ class ElementTree:
         self.create_time = os.path.getctime(file_path)
 
     def save_tree(self):
+        xsi = "http://www.w3.org/2001/XMLSchema-instance"
+        self.tree.getroot().attrib[f'{{{xsi}}}:xsd'] = "http://www.w3.org/2001/XMLSchema"
         if len(self.path.split('.')) != 2:
             raise ValueError(f'path to file {self.path} and file have to had 1 "."')
         rel_name = self.path
         new_name = self.path.split('.')[-2]+'_new'+'.'+self.path.split('.')[-1]
         old_name = self.path.split('.')[-2]+'_old'+'.'+self.path.split('.')[-1]
 
-        self.tree.write(new_name)
+        self.tree.write(new_name, encoding='utf-8', xml_declaration=True)
         try:
             os.remove(old_name)
         except:
